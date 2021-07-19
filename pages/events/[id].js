a10eb640e388c6829085c19ffe34e122d4e03150
Anukritik007 <anukritik77@gmail.com>
2021-07-19 22:35:38 +0530

    base setup

---
+++ pages/events/[id].js
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+
+function Event() {
+  return (
+    <div>
+      <Layout>
+        <h1>My Event</h1>
+      </Layout>
+    </div>
+  );
+}
+
+export default Event;
